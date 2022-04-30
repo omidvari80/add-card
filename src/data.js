91dd448009748cec5942d530784cd27f51d44d9c
fatemehomidvari <96881082+omidvari80@users.noreply.github.com>
2022-05-01 02:09:19 +0430

    first commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import imag1 from './img/imag1.jpg';
+import imag2 from './img/imag2.jpg';
+import imag3 from './img/imag3.jpg';
+const data = {
+    productData:[
+        {
+            id:1,
+            img:imag1,
+            title:'Juicy Grapes',
+            desc:'',
+            price:46,
+        },
+        {
+            id:2,
+            img:imag2,
+            title:'Red Watermelon',
+            desc:'',
+            price:56,
+        },
+        {
+            id:3,
+            img:imag3,
+            title:'Juicy Orange',
+            desc:'',
+            price:46,
+        }
+    ],
+};
+export default data ;
